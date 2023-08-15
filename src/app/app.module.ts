871bf1365602e779dd1f1eec80618082d98ac23b
yoneshinzato <yone.shinzato@montreal.com.br>
2023-08-15 17:55:47 -0300

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 
 import { appRoutes } from './routes'
-import { AppComponent }  from './components/app.component';
-import { NavBarComponent }  from './components/nav-bar.component';
-import { CatalogComponent } from "./components/catalog.component";
-import { RegisterComponent } from "./components/sign-in.component";
-import { SignInComponent } from "./components/sign-in.component";
+import { AppComponent }  from './app.component';
+import { NavBarComponent }  from './nav-bar.component';
+import { CatalogComponent } from "./catalog/catalog.component";
+import { RegisterComponent } from "./users/sign-in.component";
+import { SignInComponent } from "./users/sign-in.component";
 import { LoadingComponent } from "./components/loading-spinner.component";
 import { DataRepositoryService } from "./services/data-repository.service"
-import { AccountMenuComponent } from "./components/account-menu.component";
+import { AccountMenuComponent } from "./account-menu.component";
 
 @NgModule({
   imports:      [
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { CatalogComponent } from './components/catalog.component';
-import { RegisterComponent } from './components/sign-in.component';
-import { SignInComponent } from './components/sign-in.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { RegisterComponent } from './users/sign-in.component';
+import { SignInComponent } from './users/sign-in.component';
 
 export const appRoutes : Routes = [
   { path: 'catalog', component: CatalogComponent, },
