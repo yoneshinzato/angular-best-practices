3b4ead91bcf4f7b4a6b6983b43b198641499e17a
yoneshinzato <yone.shinzato@montreal.com.br>
2023-08-22 17:06:04 -0300

    decorating input and output properties

--- src/app/shared/loading-spinner.component.ts
+++ src/app/shared/loading-spinner.component.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core'
+import {Component, Input} from '@angular/core'
 
 @Component({
   selector: 'wb-loading-spinner',
   template: '<img *ngIf="loading" src="../../assets/images/loading.gif" />',
-  inputs: ['loading']
+
 })
 export class LoadingSpinnerComponent {
-  loading: boolean = false
+  @Input() loading: boolean = false
 }
