e7b7c12b6a1f8d43fe5ae21a304e19e4fe19ef71
yoneshinzato <yone.shinzato@montreal.com.br>
2023-08-22 16:54:30 -0300

    separating component, css and template files

--- src/app/core/nav-bar.component.ts
+++ src/app/core/nav-bar.component.ts
@@ -5,13 +5,7 @@ import { UserRepositoryService } from "./user-repository.service";
 @Component({
   selector: 'wb-nav-bar',
   styleUrls: [`./nav-bar.component.css`],
-  template: `
-    <div class="nav-bar">
-      <img class="logo" src="/assets/images/whitebeard-logo.png" alt="Whitebeard Logo" />
-      <div class="nav-item"><a [routerLink]="['/catalog']">Catalog</a></div>
-      <account-menu [user]="currentUser" (signedOut)="handleSignOut()"></account-menu>
-    </div>
-`
+  templateUrl: './nav-bar.component.html'
 })
 
 export class NavBarComponent  {
